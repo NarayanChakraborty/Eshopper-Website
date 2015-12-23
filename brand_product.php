5c09d139a6ae48c744b5f0d7e8021716373a07c1
Narayan <narayanchakraborty1993@gmail.com>
2015-12-23 10:18:14 +0600

    Pagination is Added

--- brand_product.php
+++ brand_product.php
@@ -31,7 +31,112 @@ else
 						include_once('config.php');
 						
 						
-
+						/* ===================== Pagination Code Starts ================== */
+						$adjacents = 7;
+													
+								
+						
+						
+						$brand_product=$db->prepare('select * from tbl_products where p_brand_id=?');
+						$brand_product->execute(array($brand_id));
+						$total_pages = $brand_product->rowCount();
+						if($total_pages==0)
+						{
+							?>
+								  <h1 style="text-align:center;">No Item Found</h1>
+							  <?php	
+						}
+						$targetpage = $_SERVER['PHP_SELF'];   //your file name  (the name of this file)
+						$limit =12;                                 //how many items to show per page
+						$page = @$_GET['page'];
+						if($page) 
+							$start = ($page - 1) * $limit;          //first item to display on this page
+						else
+							$start = 0;
+						
+									
+						$statement = $db->prepare("SELECT * FROM tbl_products where p_brand_id=?  ORDER BY  p_id DESC LIMIT $start, $limit");
+						$statement->execute(array($brand_id));
+						$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+						
+						
+						if ($page == 0) $page = 1;                  //if no page var is given, default to 1.
+						$prev = $page - 1;                          //previous page is page - 1
+						$next = $page + 1;                          //next page is page + 1
+						$lastpage = ceil($total_pages/$limit);      //lastpage is = total pages / items per page, rounded up.
+						$lpm1 = $lastpage - 1;   
+						$pagination = "";
+						if($lastpage > 1)
+						{   
+							$pagination .= "<div class=\"pagination\">";
+							if ($page > 1) 
+								$pagination.= "<a href=\"$targetpage?b_id=$brand_id & page=$prev\">&#171; previous</a>";
+							else
+								$pagination.= "<span class=\"disabled\">&#171; previous</span>";    
+							if ($lastpage < 7 + ($adjacents * 2))   //not enough pages to bother breaking it up
+							{   
+								for ($counter = 1; $counter <= $lastpage; $counter++)
+								{
+									if ($counter == $page)
+										$pagination.= "<span class=\"current\">$counter</span>";
+									else
+										$pagination.= "<a href=\"$targetpage?b_id=$brand_id & page=$counter\">$counter</a>";                 
+								}
+							}
+							elseif($lastpage > 5 + ($adjacents * 2))    //enough pages to hide some
+							{
+								if($page < 1 + ($adjacents * 2))        
+								{
+									for ($counter = 1; $counter < 4 + ($adjacents * 2); $counter++)
+									{
+										if ($counter == $page)
+											$pagination.= "<span class=\"current\">$counter</span>";
+										else
+											$pagination.= "<a href=\"$targetpage?b_id=$brand_id & page=$counter\">$counter</a>";                 
+									}
+									$pagination.= "...";
+									$pagination.= "<a href=\"$targetpage?b_id=$brand_id  & page=$lpm1\">$lpm1</a>";
+									$pagination.= "<a href=\"$targetpage?b_id=$brand_id  & page=$lastpage\">$lastpage</a>";       
+								}
+								elseif($lastpage - ($adjacents * 2) > $page && $page > ($adjacents * 2))
+								{
+									$pagination.= "<a href=\"$targetpage?b_id=$brand_id  & page=1\">1</a>";
+									$pagination.= "<a href=\"$targetpage?b_id=$brand_id  & page=2\">2</a>";
+									$pagination.= "...";
+									for ($counter = $page - $adjacents; $counter <= $page + $adjacents; $counter++)
+									{
+										if ($counter == $page)
+											$pagination.= "<span class=\"current\">$counter</span>";
+										else
+											$pagination.= "<a href=\"$targetpage?b_id=$brand_id & page=$counter\">$counter</a>";                 
+									}
+									$pagination.= "...";
+									$pagination.= "<a href=\"$targetpage?b_id=$brand_id & page=$lpm1\">$lpm1</a>";
+									$pagination.= "<a href=\"$targetpage?b_id=$brand_id & page=$lastpage\">$lastpage</a>";       
+								}
+								else
+								{
+									$pagination.= "<a href=\"$targetpage?b_id=$brand_id  & page=1\">1</a>";
+									$pagination.= "<a href=\"$targetpage?b_id=$brand_id  & page=2\">2</a>";
+									$pagination.= "...";
+									for ($counter = $lastpage - (2 + ($adjacents * 2)); $counter <= $lastpage; $counter++)
+									{
+										if ($counter == $page)
+											$pagination.= "<span class=\"current\">$counter</span>";
+										else
+											$pagination.= "<a href=\"$targetpage?b_id=$brand_id & page=$counter\">$counter</a>";                 
+									}
+								}
+							}
+							if ($page < $counter - 1) 
+								$pagination.= "<a href=\"$targetpage?b_id=$brand_id  & page=$next\">next &#187;</a>";
+							else
+								$pagination.= "<span class=\"disabled\">next &#187;</span>";
+							$pagination.= "</div>\n";       
+						}
+			/* ===================== Pagination Code Ends ================== */
+						
+						
 						
 						
 						
